86daf2381d7c688641e58d91e3aabeaf3cba85df
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-11-19 22:14:41 +0300

    add sql database project add user table add stored procedures for crud operations

---
+++ UserDB/dbo/StoredProcedures/spUser_Delete.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Delete]
+	@Id int
+As
+begin
+ delete from db.[user]
+ where Id = @Id;
+end
---
+++ UserDB/dbo/StoredProcedures/spUser_Get.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Get]
+	@Id int
+As
+begin
+ select Id,FirstName,LastName
+ from db.[user]
+ where Id = @Id;
+end
