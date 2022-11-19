53fcbd8d1ae9ddbcc438d22524dcf499f684377d
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-11-19 23:56:29 +0300

    add data access project

--- UserDB/Script_PostDeployment.sql
+++ UserDB/Script_PostDeployment.sql
@@ -1,6 +1,6 @@
-﻿if not exists ( select 1 from dbo.[User] )
+﻿if not exists(select 1 from [dbo].[User])
 begin
-	Insert into dbo.[User] (FirstName, LastName)
+	Insert into [dbo].[User] (FirstName, LastName)
 	values ('Tim', 'Corey'),
 		   ('Sue', 'Storm'),
 		   ('John', 'Smith'),
